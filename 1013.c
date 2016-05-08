1fe3669fab390b920858d7437615ff3e7c4d2fca
Zhang Robert <asterix314@163.com>
2016-05-08 12:50:25 +0800

    1013 bitset algorithm

--- 1013.c
+++ 1013.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <strings.h>
+
+int deduce(char rtilt[], char left[], char right[]) {
+  int light = 0;
+  int heavy = 0;
+  return 0;
+}
+
 
 int main() {
   int ncases = 0;
- 
+  int coin_flags = -1; // all bits set
+  int counterfeit = 0;
   char left[8];
   char right[8];
   char rtilt[8];
@@ -11,15 +20,16 @@ int main() {
     return 0; // input wrong
 
   for (int i=0; i<ncases; i++) {
-    // assuming nothing unexpected from input
-    
-    scanf("%s %s %s", left, right, rtilt);
-    printf("%s, %s, %s\n", left, right, rtilt);
-    scanf("%s %s %s", left, right, rtilt);
-    printf("%s, %s, %s\n", left, right, rtilt);
-    scanf("%s %s %s", left, right, rtilt);
-    printf("%s, %s, %s\n", left, right, rtilt);
-
+    for (int j=0; j<3; j++) {
+      // assuming nothing unexpected from input
+      if (3 != scanf("%s %s %s", left, right, rtilt)) 
+	return 0;
+      coin_flags |= deduce(rtilt, left, right);
+    }
+    counterfeit = ffs(coin_flags);
+    printf("%c is the counterfeit coin and it is %s.\n",
+	   'A' - 1 + ((counterfeit > 12) ? counterfeit - sizeof(int)/2 : counterfeit),
+	   (counterfeit > 12) ? "heavy" : "light");
   }
   return 0;
 }
