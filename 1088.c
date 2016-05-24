da0b1595a3c8ef3391d9a091f0a5d3223fd570c8
Robert Zhang <asterix314@163.com>
2016-05-24 19:07:38 +0800

    1088 to use automatic allocation

--- 1088.c
+++ 1088.c
@@ -4,36 +4,26 @@
 #define max2(a, b) (a) > (b) ? (a) : (b)
 #define max4(a, b, c, d) max2(max2(a, b), max2(c, d))
 
+typedef struct {
+  char row;
+  char col;
+  short npaths;
+} measure_t;
+
+int f(char nrow, char ncol) {
+  
+  short terrain[nrow][ncol];
+  measure_t measures[nrow][ncol];
+}
+
 int main() {
 
-  int ncol = 0;
-  int nrow = 0;
-
+  char nrow = 0;
+  char ncol = 0;
   
-  if (2 != scanf("%d %d", &ncol, &nrow))
+  if (2 != scanf("%c %c", &nrow, &ncol))
     return 0;
 
-  int *terrain = malloc(ncol * nrow * sizeof(int));
-
-  struct measure_t {
-    int index;
-    int npaths;
-  } *measures = malloc(ncol * nrow * sizeof(struct measure_t));
-
-  if (NULL == terrain || NULL == measures)
-    return 0;
-
-  for (int i=0; i < ncol*nrow; i++) {
-    if (1 != scanf("%d", terrain + i))
-      return 0;
-    measures[i].index = i;
-    measures[i].npaths = 0;
-  }
-
-
-
   
-  free(terrain);
-  free(measures);
   return 0;
 }
