1cd4faeb385b84713fcd0cf3879f8106a7b55fc4
Robert Zhang <asterix314@163.com>
2016-05-25 14:52:37 +0800

    minor updates

--- 1088.c
+++ 1088.c
@@ -11,19 +11,28 @@ typedef struct {
 } measure_t;
 
 int f(char nrow, char ncol) {
-  
   short terrain[nrow][ncol];
   measure_t measures[nrow][ncol];
+  for(int i=0; i<nrow; i++)
+    for(int j=0; j<ncol; j++) {
+      
+scanf("%hd",terrain+i*ncol+j);
+      measures[i][j].row = i;
+      measures[i][j].col = j;
+      measures[i][j].npaths = 0;
+printf("terrain[%c][%c] = %d\n", i, j, terrain[i][j]);
+    }
+return 0;
 }
 
 int main() {
 
   char nrow = 0;
   char ncol = 0;
-  
   if (2 != scanf("%c %c", &nrow, &ncol))
     return 0;
-
+  
+  short terrain[nrow][ncol];
   
   return 0;
 }
