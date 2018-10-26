be9b9bb2a11b90baf0c66fa3ba469ada506ee23c
asterix314 <asterix314@163.com>
2018-10-26 16:11:58 +0800

    typing ver 1

--- codechef/typing.c
+++ codechef/typing.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 const unsigned cache_size = 1031;
 const unsigned word_size = 20;
@@ -10,56 +9,44 @@ typedef struct {
     unsigned cost;
 } node_t;
 
-
 unsigned get_cost(const char str[], node_t cache[]) {
-    bool left = (str[0] == 'd' || str[0] == 'f');
-    unsigned cost = 20;
-    size_t word;
-    switch (str[0]) {
-        case 'd':
+    unsigned cost = 2;
+    size_t word =
+        (str[0] == 'd' ? 0 :
+        (str[0] == 'f' ? 1 :
+        (str[0] == 'j' ? 2 : 3)));
+    unsigned i = 1;
+    while (str[i]) {
+        word = word * 4 + 
+            (str[i] == 'd' ? 0 :
+            (str[i] == 'f' ? 1 :
+            (str[i] == 'j' ? 2 : 3)));
+        cost += ((str[i-1] < 'j') ^ (str[i] < 'j')) ? 2 : 4;
+        ++i;
     }
-
-
-    char c=str[0];
-    do {
-    }
-    
-    for (unsigned i=0; i<strlen(str), ++i) {
-        switch (strlen[i]) {
-            case 'd': 
-            case 'f'
-            case 'j'
-            case 'k'
-    }
-        
-    }
-    
-    
-    // fill cost and word
+    // search the cache
     unsigned idx = word % cache_size;
     while (cache[idx].cost && cache[idx].word != word) {
         idx = (idx + 1) % cache_size;
     }
-    if (cache[idx].cost) // found
-        return (cache[idx].cat / 2);
-    else {  // not found
+    if (cache[idx].cost) {  // found
+        return (cache[idx].cost / 2);
+    } else {                // not found
         cache[idx] = (node_t) {word, cost};
         return cost;
     }
 }
 
-
 void solve(unsigned n) {
     unsigned cost = 0;
+    node_t cache[cache_size] = {0};
+    char str[word_size+1];
     for (unsigned i=0; i<n; ++i) {
-        char str[word_size+1];
-        node_t cache[cache_size] = {0};
         scanf("%s", str);
-        time += get_cost(str, cache);
+        cost += get_cost(str, cache);
     }
-    printf("%u\n", time);
+    printf("%u\n", cost);
 }
-
 
 int main(void) {
     unsigned t=0, n=0;
