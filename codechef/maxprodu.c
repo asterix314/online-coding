dfc336442eaf5e0417c662c2f74cb3b3de3cedb1
asterix314 <asterix314@163.com>
2018-10-29 14:07:56 +0800

    minor

--- codechef/maxprodu.c
+++ codechef/maxprodu.c
@@ -2,35 +2,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int prod(int x, int k, int j) {
-    const int modulo = 1000000007;
+long long prod(long long x, long long k, long long j) {
+    const long long modulo = 1000000007;
     long long x1 = x;
     long long prod = 1;
-    for (int i=0; i<=k; ++i) {
+    for (long long i=0; i<=k; ++i) {
       prod = prod * (i==j ? 1 : (x1+i) * (x1+i-1)) % modulo;
       if (!prod) return 0; 
     }
-    return (int) prod;
+    return (long long) prod;
 }
 
-void solve(int n, int k) {
-  int j = k - (n - k*(k+1)/2) % k;
-  int x = (n + j - k*(k+1)/2) / k;
+void solve(long long n, long long k) {
+  long long j = k - (n - k*(k+1)/2) % k;
+  long long x = (n + j - k*(k+1)/2) / k;
   
   if (x>0 && j>=1 && j <=k && k*x + k*(k+1)/2 == n + j) { // solvable
     // printf("x=%d, j=%d\n", x, j);
-    printf("%d\n", prod(x, k, j));
+    printf("%lld\n", prod(x, k, j));
   } else {
     printf("-1\n");
   }
 }
 
-int main(void) {
-  int t=0;
-  scanf("%d", &t);
-  for (int i=0; i<t; ++i) {
-    int n=0, k=0;
-    scanf("%d %d", &n, &k);
+long long main(void) {
+  long long t=0;
+  scanf("%lld", &t);
+  for (long long i=0; i<t; ++i) {
+    long long n=0, k=0;
+    scanf("%lld %lld", &n, &k);
     solve(n, k);
   }
   return EXIT_SUCCESS;
